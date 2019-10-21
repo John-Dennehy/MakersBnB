d353602bc45c357cee2a38bb49ac91ac9779bd6a
Vijay Kurian <kurianvijay7@gmail.com>
2019-10-21 17:15:08 +0100

    added signup method with a validity check for emails, all tests passing

--- public/src/User.js
+++ public/src/User.js
@@ -3,3 +3,12 @@ function User() {
   this.email = "";
   this.password = "";
 };
+
+User.prototype.SignUp = function(username, email, password) {
+  if ((/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+    return true
+  } else {
+    // console.log("fl")
+    throw "Please enter a valid email address"
+  }
+};
