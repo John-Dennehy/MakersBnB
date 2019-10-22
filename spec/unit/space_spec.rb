b05938b50cb7ad943b17396e8400dc11405fa6eb
Jessica Marais <jessica_marais@hotmail.com>
2019-10-22 12:19:09 +0100

    Datamapper setup complete

--- spec/unit/space_spec.rb
+++ spec/unit/space_spec.rb
@@ -1,36 +1,24 @@
-require 'date'
-# require 'space'
+require 'space'
 
 require_relative '../../datamapper_setup.rb'
 
 describe Space do
 subject(:space) { described_class.create(id: 1, name: 'London', description: 'Beautiful place to stay', ppn: 5000 ) }
 
-  # describe 'initialization' do
-  #
-  #   it 'starts with an id' do
-  #     expect(space.id).to eq 1
-  #   end
-  #
-  #
-  #   it 'starts with a name' do
-  #     expect(space.name).to eq "London"
-  #   end
-  #
-  #   it 'starts with a description' do
-  #     expect(space.description).to eq "Beautiful place to stay"
-  #   end
-  #
-  #   it 'starts with a price per night' do
-  #     expect(space.ppn).to eq 5000
-  #   end
-  # end
+describe ".create" do
+ it "saves space to db returning a new object with the DB id" do
+   this_space = Space.create(name: "my_place", description: "A nice place to stay", ppn: 4000)
+   expect(this_space.name).to eq "my_place"
+   expect(this_space.description).to eq "A nice place to stay"
+   expect(this_space.ppn).to eq 4000
+ end
+end
 
   # describe 'add dates space is unavailable' do
   #
   #   it 'dates_unavailable array is not empty when date added' do
   #     date = Date.new(2019,12,20)
-  #     space.add_date_unavailable(date: date)
+  #     space.add_date_unavailabgit ble(date: date)
   #     expect(space.dates_unavailable).not_to be_empty
   #   end
   #
@@ -41,5 +29,5 @@ subject(:space) { described_class.create(id: 1, name: 'London', description: 'Be
   #   end
   # end
 
-  
+
 end
