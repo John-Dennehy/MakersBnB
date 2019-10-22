10ec30f9d8659f8f2efaede2df82af9de1ec0684
Jessica Marais <jessica_marais@hotmail.com>
2019-10-22 12:28:20 +0100

    User can call .create or .method on Space

--- spec/unit/space_spec.rb
+++ spec/unit/space_spec.rb
@@ -5,14 +5,26 @@ require_relative '../../datamapper_setup.rb'
 describe Space do
 subject(:space) { described_class.create(id: 1, name: 'London', description: 'Beautiful place to stay', ppn: 5000 ) }
 
-describe ".create" do
- it "saves space to db returning a new object with the DB id" do
-   this_space = Space.create(name: "my_place", description: "A nice place to stay", ppn: 4000)
-   expect(this_space.name).to eq "my_place"
-   expect(this_space.description).to eq "A nice place to stay"
-   expect(this_space.ppn).to eq 4000
- end
-end
+  describe ".create" do
+   it "saves space to db returning a new object with the DB id" do
+     this_space = Space.create(name: "my_place", description: "A nice place to stay", ppn: 4000)
+     expect(this_space.name).to eq "my_place"
+     expect(this_space.description).to eq "A nice place to stay"
+     expect(this_space.ppn).to eq 4000
+   end
+  end
+
+  describe '.all' do
+    it 'lists all of the saved spaces' do
+      create_space = Space.create(name: "my_place", description: "A nice place to stay", ppn: 4000)
+      spaces = Space.all
+
+      expect(spaces).to include create_space
+      expect(spaces.last.name).to eq 'my_place'
+      expect(spaces.last.description).to eq 'A nice place to stay'
+      expect(spaces.last.ppn).to eq 4000
+    end
+  end
 
   # describe 'add dates space is unavailable' do
   #
